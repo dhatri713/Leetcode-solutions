0fa76c9da43cec7384304916a574fd9d5550f3e2
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-07-09 13:38:04 +0530

    Time: 22 ms (46.40%), Space: 27.4 MB (40.16%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
+    void reverse(vector<int>& nums, int l, int r){
+        while (l <= r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
+        // OPTIMAL SOLN TIME WISE
+
+        // k = k % nums.size();
+        // vector<int> temp(k);
+        // for (int i = (nums.size() - k); i < nums.size(); i++){
+        //     temp[i - (nums.size() - k)] = nums[i];
+        // }
+        // for (int i = (nums.size() - k - 1); i >= 0; i--){
+        //     nums[i + k] = nums[i];
+        // }
+        // for (int i  = 0; i < k; i++){
+        //     nums[i] = temp[i];
+        // }
+
+        // OPTIMAL SOLUTION space wise
         k = k % nums.size();
-        vector<int> temp(k);
-        for (int i = (nums.size() - k); i < nums.size(); i++){
-            temp[i - (nums.size() - k)] = nums[i];
-        }
-        for (int i = (nums.size() - k - 1); i >= 0; i--){
-            nums[i + k] = nums[i];
-        }
-        for (int i  = 0; i < k; i++){
-            nums[i] = temp[i];
-        }
+        reverse(nums, 0, (nums.size() - k - 1));
+        reverse(nums, nums.size() - k, nums.size() - 1);
+        reverse(nums, 0 , nums.size() -1);
     }
 };
