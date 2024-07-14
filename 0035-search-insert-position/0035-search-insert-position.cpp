a2ce98b893aa3d8ab6ba80244fd6d63825979b51
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-07-14 19:33:37 +0530

    Time: 0 ms (100.00%), Space: 12.5 MB (9.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -3,21 +3,37 @@ public:
     int lower_bound(vector<int> v, int x){
     int low = 0;
     int high = v.size() - 1;
-    int curr_floor_ind = -1;
+    int curr_floor_ind = v.size();
 
+    // while (low <= high){
+    //     int mid = low + (high - low)/2;
+    //     if (v[mid] == x) {
+    //         return mid;
+    //     }
+    //     else if(v[mid] < x){
+    //         low = mid + 1;
+    //         curr_floor_ind = max(mid, curr_floor_ind);            }
+    //     else {
+    //         high = mid - 1;
+    //     }
+    // }
+    // return curr_floor_ind + 1;
+        
+    // better way to write this
+        
     while (low <= high){
         int mid = low + (high - low)/2;
-        if (v[mid] == x) {
-            return mid;
-        }
-        else if(v[mid] < x){
-            low = mid + 1;
-            curr_floor_ind = max(mid, curr_floor_ind);            }
-        else {
+        if (v[mid] >= x){
+            curr_floor_ind = mid;
+            
+            // look for small index on the left
             high = mid - 1;
         }
+        else {
+            low = mid + 1;
+        }
     }
-    return curr_floor_ind + 1;
+        return curr_floor_ind;
 }
     int searchInsert(vector<int>& nums, int target) {
         return lower_bound(nums, target);
