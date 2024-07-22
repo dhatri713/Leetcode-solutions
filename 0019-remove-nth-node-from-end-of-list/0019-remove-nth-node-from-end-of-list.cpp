21ee95d09a0848d3d5531cd0491f9521c7e4a66e
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-07-22 05:52:15 +0530

    Time: 8 ms (7.61%), Space: 14.7 MB (6.19%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,31 +11,65 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // brute force 
+        
+//         if (head == nullptr || (head->next == nullptr && n == 1)) return nullptr;
+//         if (n == 0) return head;
+//         int length = 0;
+//         ListNode* mover = head;
+//         while(mover != nullptr){
+//             length++;
+//             mover = mover->next;
+//         }
+        
+//         mover = head;
+        
+//         if (length == n){
+//             head = head->next;
+//             delete mover;
+//             return head;
+//         }
+        
+//         int cnt = (length - n);
+//         while (cnt > 1){
+//             mover = mover->next;
+//             cnt--;
+//         }
+//         ListNode* temp = (mover->next) ? mover->next : nullptr;
+//         mover->next = (temp->next) ? temp->next : nullptr;
+//         delete temp;
+//         return head;
+        
+        // optimal - using slow and fast pointers
         if (head == nullptr || (head->next == nullptr && n == 1)) return nullptr;
         if (n == 0) return head;
-        int length = 0;
-        ListNode* mover = head;
-        while(mover != nullptr){
-            length++;
-            mover = mover->next;
+        
+        ListNode* fast = head;
+        ListNode* slow = head;
+        int cnt = n;
+        
+        while (cnt > 0){
+            fast = fast->next;
+            cnt --;
         }
         
-        mover = head;
-        
-        if (length == n){
+        if (fast == nullptr){
             head = head->next;
-            delete mover;
+            delete fast;
+            delete slow;
             return head;
         }
         
-        int cnt = (length - n);
-        while (cnt > 1){
-            mover = mover->next;
-            cnt--;
+        while (fast->next != nullptr){
+            fast = fast->next;
+            slow = slow->next;
         }
-        ListNode* temp = (mover->next) ? mover->next : nullptr;
-        mover->next = (temp->next) ? temp->next : nullptr;
+        
+        ListNode* temp = slow->next;
+        slow->next = temp->next;
         delete temp;
+        // delete fast;
+        // delete slow;
         return head;
     }
 };
