90929920549276b943a03b1c0119801fac0692e1
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-07-23 13:16:51 +0530

    Time: 39 ms (58.28%), Space: 17.4 MB (22.50%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,61 +9,22 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int len1 = 0;
-        int len2 = 0;
-        
         ListNode* temp1 = headA;
         ListNode* temp2 = headB;
         
-        while(temp1 != nullptr && temp2 != nullptr){
-            len1++;
-            len2++;
-            temp1 = temp1->next;
-            temp2 = temp2->next;
-        }
-        
-        while(temp1 != nullptr){
-            len1++;
-            temp1 = temp1->next;
-        }
-        
-        while(temp2 != nullptr){
-            len2++;
-            temp2 = temp2->next;
-        }
-        
-        cout << len1 << " " << len2 << endl;
-        
-        temp1 = headA;
-        temp2 = headB;
-        if (len1 > len2){
-            int diff = len1 - len2;
-            cout << diff << endl;
-            //temp1 = headA;
-            while (diff > 0){
-                temp1 = temp1->next;
-                diff--;
+        while (temp1 != nullptr || temp2 != nullptr){
+            if (temp1 == nullptr){
+                temp1 = headB;
             }
-        }
-        
-        else {
-            int diff = len2 - len1;
-            cout << diff << endl;
-            //temp2 = headB;
-            while (diff > 0){
-                temp2 = temp2->next;
-                diff--;
+            if (temp2 == nullptr){
+                temp2 = headA;
             }
-        }
-        
-        cout <<"after same level" << temp1->val << " " << temp2->val << endl;
-        
-        while (temp1 != nullptr && temp2 != nullptr){
+            
             if (temp1 == temp2) return temp1;
+            
             temp1 = temp1->next;
             temp2 = temp2->next;
         }
-        
         return NULL;
     }
 };
