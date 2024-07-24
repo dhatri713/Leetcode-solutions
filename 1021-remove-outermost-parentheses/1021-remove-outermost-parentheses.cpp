f23ec8885b13b8211131cd1a204661d57c58df29
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-07-24 10:28:31 +0530

    Time: 2 ms (70.84%), Space: 8.3 MB (27.44%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<int> st;
+        // stack<int> st;
+        // string ans = "";
+        // for (int i = 0; i < s.length(); i++){
+        //     if (st.empty()) st.push(s[i]);
+        //     else if (!st.empty() && s[i] == ')'){
+        //         st.pop();
+        //         if (!st.empty()) ans += s[i];
+        //     }
+        //     else {
+        //         st.push(s[i]);
+        //         ans += s[i];
+        //     }
+        // }
+        // return ans;
+        
         string ans = "";
+        int counter = 0;
         for (int i = 0; i < s.length(); i++){
-            if (st.empty()) st.push(s[i]);
-            else if (!st.empty() && s[i] == ')'){
-                st.pop();
-                if (!st.empty()) ans += s[i];
+            if (counter == 0 and s[i] == '(') counter += 1;
+            else if (counter == 1 && s[i] == ')') counter -= 1;
+            else if (counter != 0 && s[i] == '('){
+                counter += 1;
+                ans += '(';
             }
-            else {
-                st.push(s[i]);
-                ans += s[i];
+            else if (counter != 0 && s[i] == ')'){
+                counter -= 1;
+                ans += ')';
             }
         }
         return ans;
