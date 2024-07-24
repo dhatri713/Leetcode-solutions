969aa63d3e3aa477da34bb80498f793eb421d781
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-07-24 20:59:11 +0530

    Time: 4 ms (70.09%), Space: 14.6 MB (12.52%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,22 +17,51 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mpp;
+//         unordered_map<Node*, Node*> mpp;
+//         Node* temp = head;
+//         while (temp != nullptr){
+//             Node* new_node = new Node(temp->val);
+//             mpp[temp] = new_node;
+//             temp = temp->next;
+//         }
+        
+//         temp = head;
+//         while (temp != nullptr){
+//             Node* next = mpp[temp->next];
+//             Node* random = mpp[temp->random];
+//             mpp[temp]->next = next;
+//             mpp[temp]->random = random;
+//             temp = temp->next;
+//         }
+//         return mpp[head];
+        
         Node* temp = head;
         while (temp != nullptr){
-            Node* new_node = new Node(temp->val);
-            mpp[temp] = new_node;
-            temp = temp->next;
+            Node* copy = new Node(temp->val);
+            copy->next = temp->next;
+            temp->next = copy;
+            temp = temp->next->next;
         }
         
         temp = head;
         while (temp != nullptr){
-            Node* next = mpp[temp->next];
-            Node* random = mpp[temp->random];
-            mpp[temp]->next = next;
-            mpp[temp]->random = random;
+            if (temp->random == nullptr) temp->next->random = nullptr;
+            else {
+                temp->next->random = temp->random->next;
+            }
+            temp = temp->next->next;
+        }
+        
+        Node* dummy = new Node(-1);
+        Node* res = dummy;
+        temp = head;
+        while (temp != nullptr){
+            res->next = temp->next;
+            temp->next = temp->next->next;
+            res = res->next;
             temp = temp->next;
         }
-        return mpp[head];
+        
+        return dummy->next;
     }
 };
