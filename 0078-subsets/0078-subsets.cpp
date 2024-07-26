0ff0b1ddfb8451c67ea8ed1ac6422906ed52e066
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-07-26 10:49:45 +0530

    Time: 3 ms (46.28%), Space: 15.4 MB (7.22%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void generate(int i, vector<int> nums, vector<vector<int>>& subsets, vector<int> subset){
+        if (i >= nums.size()){
+            subsets.push_back(subset);
+            return;
+        }
+        
+        // decision to include nums[i]
+        subset.push_back(nums[i]);
+        generate(i + 1, nums, subsets, subset);
+        
+        // decision to not include nums[i]
+        subset.pop_back();
+        generate(i + 1, nums, subsets, subset);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> subset;
+        generate(0, nums, ans, subset);
+        return ans;
+    }
+};
