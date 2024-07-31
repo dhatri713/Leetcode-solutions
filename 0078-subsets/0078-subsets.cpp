dbea42381e482b70895d419a0da6e3cb51455ac9
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-07-31 05:30:10 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (46.34%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -16,9 +16,21 @@ public:
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
+        // vector<vector<int>> ans;
+        // vector<int> subset;
+        // generate(0, nums, ans, subset);
+        // return ans;
+        
+        int num_subsets = 1 << nums.size();
         vector<vector<int>> ans;
-        vector<int> subset;
-        generate(0, nums, ans, subset);
+        
+        for (int i = 0; i < num_subsets; i++){
+            vector<int> subset;
+            for (int j = 0; j < nums.size(); j++){
+                if ((i & (1 << j)) != 0) subset.push_back(nums[j]);
+            }
+            ans.push_back(subset);
+        }
         return ans;
     }
 };
