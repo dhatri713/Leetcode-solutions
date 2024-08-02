f8794972bc6bfeefd9a2af4e6ef52809ec9bbfbb
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-08-02 09:29:02 +0530

    Time: 4 ms (14.38%), Space: 8.6 MB (37.09%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -9,38 +9,61 @@ public:
     }
     
     void push(int x) {
-        if (s1.empty()) s1.push(x);
-        else {
-            while (!s1.empty()){
-                int val = s1.top();
-                s1.pop();
-                s2.push(val);
-            }
+//         if (s1.empty()) s1.push(x);
+//         else {
+//             while (!s1.empty()){
+//                 int val = s1.top();
+//                 s1.pop();
+//                 s2.push(val);
+//             }
             
-            s1.push(x);
+//             s1.push(x);
             
-            while (!s2.empty()){
-                int val = s2.top();
-                s2.pop();
-                s1.push(val);
-            }
-        }
+//             while (!s2.empty()){
+//                 int val = s2.top();
+//                 s2.pop();
+//                 s1.push(val);
+//             }
+//         }
+        
+        s1.push(x);
     }
     
     int pop() {
-        if (s1.empty()) return -1;
-        int val = s1.top();
-        s1.pop();
-        return val;
+        // if (s1.empty()) return -1;
+        // int val = s1.top();
+        // s1.pop();
+        // return val;
+        
+        if (!s2.empty()) {
+            int val = s2.top();
+            s2.pop();
+            return val;
+        }
+        
+        while (!s1.empty()){
+            int val = s1.top();
+            s2.push(val);
+            s1.pop();
+        }
+        
+        int ans = s2.top();
+        s2.pop();
+        return ans;
     }
     
     int peek() {
-        if (s1.empty()) return -1;
-        return s1.top();
+        if (!s2.empty()) return s2.top();
+        while (!s1.empty()){
+            int val = s1.top();
+            s2.push(val);
+            s1.pop();
+        }
+        return s2.top();  
     }
     
     bool empty() {
-        return s1.empty();
+        return s1.empty() && s2.empty();
     }
 };
 
