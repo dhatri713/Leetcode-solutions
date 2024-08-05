cfce1aaccce2af4ba2b84591c426e84ecf83cd0b
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-08-05 10:02:24 +0530

    Time: 7 ms (87.21%), Space: 22.8 MB (45.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        //approach 1
-        vector<int> prefix(height.size(), 0);
-        vector<int> suffix(height.size(), 0);
-        int volume = 0;
+//         //approach 1
+//         vector<int> prefix(height.size(), 0);
+//         vector<int> suffix(height.size(), 0);
+//         int volume = 0;
         
-        prefix[0] = height[0];
-        for (int i = 1; i < height.size(); i++){
-            prefix[i] = max(prefix[i - 1], height[i]);
-        }
+//         prefix[0] = height[0];
+//         for (int i = 1; i < height.size(); i++){
+//             prefix[i] = max(prefix[i - 1], height[i]);
+//         }
         
-        suffix[height.size() - 1] = height[height.size() - 1];
-        for (int i = height.size() - 2; i >= 0; i--){
-            suffix[i] = max(suffix[i + 1], height[i]);
-        }
+//         suffix[height.size() - 1] = height[height.size() - 1];
+//         for (int i = height.size() - 2; i >= 0; i--){
+//             suffix[i] = max(suffix[i + 1], height[i]);
+//         }
         
-        for (int i = 0; i < height.size(); i++){
-            if (height[i] < prefix[i] && height[i] < suffix[i]){
-                volume += (min(prefix[i], suffix[i]) - height[i]);
+//         for (int i = 0; i < height.size(); i++){
+//             if (height[i] < prefix[i] && height[i] < suffix[i]){
+//                 volume += (min(prefix[i], suffix[i]) - height[i]);
+//             }
+//         }
+        
+//         return volume;
+        
+        // approach 2
+        int n = height.size();
+        int lm = 0;
+        int rm = 0;
+        int l = 0;
+        int r = n - 1;
+        int vol = 0;
+        
+        while (l < r){
+            if (height[l] < height[r]){
+                if (lm > height[l]) vol += (lm - height[l]);
+                else lm = height[l];
+                l++;
+            }
+            else {
+                if (rm > height[r]) vol += (rm - height[r]);
+                else rm = height[r];
+                r--;
             }
         }
         
-        return volume;
+        return vol;
     }
 };
