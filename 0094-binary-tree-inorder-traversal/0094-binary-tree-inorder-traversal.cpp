13d9b15a01c814da56a49f59a704476080aa240f
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-09-06 06:45:36 +0530

    Time: 2 ms (42.08%), Space: 10.1 MB (24.61%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -20,6 +20,22 @@ public:
 }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
-    return in_order(root, res);
-    }
+    //return in_order(root, res);
+        if (!root) return res;
+        stack<TreeNode*> st;
+        TreeNode* node = root;
+        while (true){
+            if (node){
+                st.push(node);
+                node = node->left;
+            }
+            else {
+                if (st.empty()) break;
+                node = st.top();
+                st.pop();
+                res.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return res;}
 };
