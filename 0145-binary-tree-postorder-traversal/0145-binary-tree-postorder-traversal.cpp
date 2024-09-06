4a85619778a3aba7ead008d9ed5dbc2249a5a62f
Dhatri Manaswini <159056575+dhatri713@users.noreply.github.com>
2024-09-06 10:25:54 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (86.70%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -23,23 +23,52 @@ vector<int> postorderTraversal(TreeNode* root) {
     vector<int> res;
     // return pre_order(root, res);
     
-    stack<TreeNode*> st1;
-    stack<TreeNode*> st2;
+//     stack<TreeNode*> st1;
+//     stack<TreeNode*> st2;
     
     if (!root) return res;
-    st1.push(root);
-    while (!st1.empty()){
-        TreeNode* node = st1.top();
-        st1.pop();
-        if (node->left) st1.push(node->left);
-        if (node->right) st1.push(node->right);
-        st2.push(node);
-    }
+//     st1.push(root);
+//     while (!st1.empty()){
+//         TreeNode* node = st1.top();
+//         st1.pop();
+//         if (node->left) st1.push(node->left);
+//         if (node->right) st1.push(node->right);
+//         st2.push(node);
+//     }
     
-    while (!st2.empty()){
-        TreeNode* temp = st2.top();
-        res.push_back(temp->val);
-        st2.pop();
+//     while (!st2.empty()){
+//         TreeNode* temp = st2.top();
+//         res.push_back(temp->val);
+//         st2.pop();
+//     }
+//     return res;
+    
+    TreeNode* curr = root;
+    TreeNode* temp;
+    
+    stack<TreeNode*> st;
+    
+    while (curr != nullptr || !st.empty()){
+        if (curr != nullptr){
+            st.push(curr);
+            curr = curr->left;
+        }
+        else {
+            temp = st.top()->right;
+            if (!temp){
+                temp = st.top();
+                st.pop();
+                res.push_back(temp->val);
+                while (!st.empty() && temp == st.top()->right){
+                    temp = st.top();
+                    st.pop();
+                    res.push_back(temp->val);
+                }
+            }
+            else {
+                curr = temp;
+            }
+        }
     }
     return res;
 }
